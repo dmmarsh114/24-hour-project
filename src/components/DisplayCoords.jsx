250c8b589b88185a96a59a221b205723f59b50e9
unknown <gaffney339911@gmail.com>
2020-03-26 21:46:10 -0500

    got location to fetch

--- src/components/DisplayCoords.jsx
+++ src/components/DisplayCoords.jsx
@@ -4,7 +4,7 @@ export default function DisplayCoords(props) {
     return (
         <div>
             <p>Latitude: {props.lat}</p>
-            <p>Longitude: {props.long}</p>
+            <p>Longitude: {props.lon}</p>
         </div>
     )
 }
