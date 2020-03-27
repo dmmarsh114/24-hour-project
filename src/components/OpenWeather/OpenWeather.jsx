dce8d4df6fa11c8893bd571096511449951ba541
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-03-27 11:31:39 -0400

    converting temp to celsius

--- src/components/OpenWeather/OpenWeather.jsx
+++ src/components/OpenWeather/OpenWeather.jsx
@@ -2,32 +2,35 @@ import React from 'react';
 import OpenWeatherDisplay from './OpenWeatherDisplay';
 
 export default class Fetch extends React.Component {
-    // constructor (props){
-    //     super(props)
+    constructor(props) {
+        super(props)
 
-    // }
+        this.state = {
+            temp: '',
+        }
+    }
 
-    componentWillMount () {
+    componentWillMount() {
         console.log("component will mount");
     };
 
-    componentDidMount (){
+    componentDidMount() {
         console.log("component mounted");
 
         fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.props.lat}&lon=${this.props.lon}&units=imperial&appid=eb4d2ade0bb45cb0441c9d29ed41aa53`)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                this.setState({
+                })
+            })
+    };
 
-        // fetch('api.openweathermap.org/data/2.5/weather?lat=35&lon=139=eb4d2ade0bb45cb0441c9d29ed41aa53')
-        .then(response => response.json())
-        .then(json => {
-        console.log(json);
-    })
-};
 
-
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                {/* <OpenWeatherDisplay /> */}
+                <OpenWeatherDisplay temp={this.state.temp} />
             </div>
         )
     }
