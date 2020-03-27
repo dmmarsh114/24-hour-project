55680ea1edd1288637f285751730657df8aa97de
wchestnuti27 <wchestnuti27@users.noreply.github.com>
2020-03-27 09:39:17 -0400

    Updated code in OpenWeather and Geolocate component

--- src/components/OpenWeather/OpenWeather.jsx
+++ src/components/OpenWeather/OpenWeather.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import OpenWeatherDisplay from './OpenWeatherDisplay';
 
 export default class Fetch extends React.Component {
-    constructor (props){
-        super(props)
+    // constructor (props){
+    //     super(props)
 
-        this.state = {
-            lat: '',
-            lon: ''
-        }
-    }
+    // }
 
     componentWillMount () {
         console.log("component will mount");
@@ -18,18 +14,12 @@ export default class Fetch extends React.Component {
     componentDidMount (){
         console.log("component mounted");
 
-        // fetch(`http://api.openweathermap.org/data/2.5/find?lat=${this.state.lat}&lon=${this.state.lon}&cnt=10=eb4d2ade0bb45cb0441c9d29ed41aa53`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.props.lat}&lon=${this.props.lon}&appid=eb4d2ade0bb45cb0441c9d29ed41aa53`)
 
-        fetch('api.openweathermap.org/data/2.5/weather?lat=35&lon=139=eb4d2ade0bb45cb0441c9d29ed41aa53')
+        // fetch('api.openweathermap.org/data/2.5/weather?lat=35&lon=139=eb4d2ade0bb45cb0441c9d29ed41aa53')
         .then(response => response.json())
         .then(json => {
-            console.log(json);
-            this.setState({
-            lat: json.lat,
-            lon: json.lon
-        })
-        console.log(this.state.lat);
-        console.log(this.state.lon);
+        console.log(json);
     })
 };
 
@@ -37,7 +27,7 @@ export default class Fetch extends React.Component {
     render(){
         return(
             <div>
-                <OpenWeatherDisplay />
+                {/* <OpenWeatherDisplay /> */}
             </div>
         )
     }
