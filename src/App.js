2b04825a2b21c9bbb6bf5c681f383b8533de1546
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-03-26 20:14:42 -0400

    Geolocate comp now passes coords down to other comps

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 
 import Geolocate from './components/Geolocate';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>this is team 6's incredible app that was made in 24 hours!</h1>
-      <Geolocate />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>this is team 6's incredible app that was made in 24 hours!</h1>
+        <Geolocate />
+      </div>
+    );
+  }
 }
 
 export default App;
-
